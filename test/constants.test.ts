3467f0de1e9248a429f4cda26a9e7276ffffddbc
wanglonghong <30057497+wanglonghong@users.noreply.github.com>
2021-02-28 21:34:30 +0800

    first deployment

---
+++ test/constants.test.ts
@@ -0,0 +1,20 @@
+import { INIT_CODE_HASH } from '../src/constants'
+
+//import { bytecode } from '@uniswap/v2-core/build/UniswapV2Pair.json'
+//import { keccak256 } from '@ethersproject/solidity'
+
+// this _could_ go in constants, except that it would cost every consumer of the sdk the CPU to compute the hash
+// and load the JSON.
+//const COMPUTED_INIT_CODE_HASH = keccak256(['bytes'], [`0x${bytecode}`])
+
+describe('constants', () => {
+  describe('INIT_CODE_HASH', () => {
+    // it('matches computed bytecode hash', () => {
+    //   expect(COMPUTED_INIT_CODE_HASH).toEqual(INIT_CODE_HASH)
+    // })
+    //TODO: Replace with real CombustPair.json
+    it('matches computed bytecode hash', () => {
+      expect(INIT_CODE_HASH).toEqual('0xe3c165df86b0f8435d59ef58371c7f13bafaf40684fac9cd077a462d9a482331')
+    })
+  })
+})
